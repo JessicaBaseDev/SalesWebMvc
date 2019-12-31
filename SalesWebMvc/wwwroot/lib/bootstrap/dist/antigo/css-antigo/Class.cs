703174bed724edf400781699ef831053dde4152d
JessicaBaseDev <jessica@basedev.com.br>
2019-12-31 11:45:54 -0300

    alteração de bootstrap

---
+++ SalesWebMvc/wwwroot/lib/bootstrap/dist/antigo/css-antigo/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.wwwroot.lib.bootstrap.dist.css
+{
+    public class Class
+    {
+    }
+}
